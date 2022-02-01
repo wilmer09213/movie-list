293ca8e78f58449e475e20f84cba7a8adc67d152
unknown <wilmer09213@gmail.com>
2022-02-01 15:26:08 +0800

    remove static build without if statement

--- server.js
+++ server.js
@@ -58,19 +58,6 @@ app.delete('/delete/:id', function(req, res) {
     })
 });
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-}
-
-
-
-app.use(express.static(path.join(__dirname, "./client/build")))
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build"));
-});
 
 
 if(process.env.NODE_ENV === "production") {
